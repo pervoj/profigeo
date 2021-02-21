68d192655f7f263ed127776e9afb42096c3cf617
Vojtěch Perník <pervoj@gmx.com>
2021-02-21 12:10:54 +0100

    Add footer to each page + Change JS system + Add by JS changing footer year

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function menuToggle() {
-    var menu = document.getElementById('nav-menu');
-    menu.classList.toggle('nav-hidden');
+// Define menu toggle function:
+document.getElementById("nav-toggle").onclick = function() {
+    var menu = document.getElementById("nav-menu");
+    menu.classList.toggle("nav-hidden");
+}
+
+
+
+// Change copyright year:
+var copyrightYear = document.getElementById("copyright-year").innerText;
+var currentYear = new Date().getFullYear();
+if (copyrightYear != currentYear) {
+    document.getElementById("copyright-year").innerText = copyrightYear + " - " + currentYear;
 }
