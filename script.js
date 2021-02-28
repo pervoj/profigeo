2e12163e7b2ec6d9815ccc7be5694982a98c1a04
Vojtěch Perník <pervoj@gmx.com>
2021-02-28 16:49:20 +0100

    Add accordion support

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ document.getElementById("nav-toggle").onclick = function() {
 
 
 
+// Define accordion opening:
+var acc = document.getElementsByClassName("accordion");
+for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("accordion-active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.classList.contains("accordion-panel")) {
+            panel.classList.toggle("accordion-panel-hidden");
+        }
+    });
+}
+
+
+
 // Change copyright year:
 var copyrightYear = document.getElementById("copyright-year").innerText;
 var currentYear = new Date().getFullYear();
